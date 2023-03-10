228d9355df44d2b16963fd8d88eaf9687367da90
babatunde mustapha <mustapha@AdminisatorsMBP.verizon.net>
2023-03-09 21:42:13 -0500

    implement adding of contact

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useEffect, useState} from 'react';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// handles input 
+// handles the submit
+function AddNewPerson(props){
+
+    const [person, setPerson] = useState('')
+
+    function handlesubmit(e){
+        // this is the only part in this component that mentions props....
+        //this means whenever this com....
+        //supply person to props.handlesubmit
+        props.handlesubmit(person)
+
+
+        // manage the click
+        // it should not go next page after clicking
+        e.preventDefault()
+
+    }
+    function handleChange(e){
+        setPerson(e.target.value)
+    }
+
+    return <div>
+
+        <form>
+            <input type="text"
+          placeholder="Enter the person"
+          onChange={handleChange}
+          value={person} />
+
+            <button onClick={handlesubmit}> Add New Contact </button>
+        </form>
+
+
+
+           </div>
+
+
+}
+
+function ListContact(props){
+    const arr = props.data;
+    const listItems = arr.map((val, key) => <li key={key}> {val} </li> );
+    return <ul>{listItems}</ul>;
+    //const listItems = arr.map((v, k) => <li key={key}>{val}</li>);
+}
+
+
+
+function ContactManager(props){
+    const [contact, setContact] = useState(props.data);
+
+    function addperson(name){
+        setContact([...contact, name])
+    }
+// so basically that was the only place where we called the addperson
+// after calling the addperson, we instruct it to pick it input from handlesubmit of addnewperson
+// the handle submit of the addnewperson was created and informed that it will be passing updater person
+// to the props.handleSubmit 
+//console.log(contact)
+   return <div>
+   <ListContact  data={contact}/>
+   <AddNewPerson handlesubmit={addperson}/>
+
+    </div>
+
+
+}
+
+const names = ["Bolu", "Gbenga", "Ademola", "Tolulope"];
+
+const me = <ContactManager data={names} />;
+
+ReactDOM.render(me , document.getElementById("root") )
